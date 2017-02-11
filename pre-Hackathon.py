571b18ad5723f397cd925060cf60a744d1db01d2
hp-PC\hp <rabac1998@gmail.com>
2017-02-11 12:59:05 +0700

    commit

--- pre-Hackathon.py
+++ pre-Hackathon.py
@@ -1,8 +1,12 @@
 from flask import Flask,render_template,redirect,url_for,request
 import mongoengine
 from mongoengine import *
+import os
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+images_folder = os.path.join(APP_ROOT, 'static/images/')
 
+unit = []
 
 connect(
     "pre_hackathon",
@@ -18,22 +22,17 @@ class UserSignUp(Document):
     password = StringField()
 
 
-class UserSignIn(Document):
-    username     = StringField()
-    password = StringField()
-
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def homepage():
     return render_template("homepage.html")
 
 
 @app.route('/sign')
 def sign():
-    return render_template("sign-up.html")
+    return render_template("sign.html")
 
 @app.route('/create')
 def create():
@@ -42,30 +41,46 @@ def create():
 @app.route('/signup',methods=["GET","POST"])
 def signup():
     if request.method =="GET" :
-        return render_template("sign-up.html")
+        return render_template("signup.html")
     elif request.method == "POST":
+        namex     = request.form["Name"]
         usernamex = request.form["userSignUp"]
-        namex     = request.form["userName"]
         passwordx = request.form["SignUpPassw"]
-        user = UserSignUp(name=namex,username= usernamex, password=passwordx)
-        user.save()
+        user = UserSignUp.objects(username=usernamex).first()
+        if user is None:
+            user = UserSignUp(name=namex,username= usernamex, password=passwordx)
+            user.save()
+        else: print('tài khoản đã tồn tại')
         return ("Thank You")
+
+images_folder = os.path.join(APP_ROOT, 'static/images/')
+
+
 
 
 @app.route('/signin',methods=["GET","POST"])
 def signin():
     if request.method =="GET" :
-        return render_template("sign-up.html")
+        return render_template("signin.html")
     elif request.method == "POST":
         usernamex = request.form["userSignIn"]
         passwordx = request.form["SignInPassw"]
         user = UserSignUp.objects(username=usernamex).first()
         if (user is not None) and (passwordx == user.password):
             print("ahihi")
-        else: print("tài khoản hoặc mật khẩu không chính xác")
+        else:
+            return render_template("signin.html")
         print(user)
-        return("OK")
         # if
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/index1')
+def HelloHuy():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run()
